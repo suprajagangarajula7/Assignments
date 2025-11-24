712d308468fd91ea93b59de6e195bdfa1c6ee9ca
suprajagangarajula7 <suprajagangarajula7@gmail.com>
2025-11-24 12:44:01 +0530

    Destructuring

---
+++ Week3Day1/Q3/Destructuring.js
@@ -0,0 +1,14 @@
+const people = [ { name: "Alice", address: { city: "New York", street: { name: "Broadway", number: 123 } } }, 
+{ name: "Bob", address: { city: "Los Angeles", street: { name: "Sunset Boulevard", number: 456 } } } ];
+const result= people.map(person => {
+  const { 
+    name: personName,
+    address: {
+      city,
+      street: { name: streetName }
+    }
+  } = person;
+
+  return `${personName} lives in ${city} on ${streetName}`;
+});
+console.log(result);
