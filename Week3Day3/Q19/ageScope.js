b0c8a23ab5650184778fb93b653d47db2079b764
suprajagangarajula7 <suprajagangarajula7@gmail.com>
2025-11-25 12:13:46 +0530

    ageScope

---
+++ Week3Day3/Q19/ageScope.js
@@ -0,0 +1,15 @@
+var age=20; 
+function displayAge(){
+  console.log("Displayed Age", age)
+}
+function changeAge(){
+  age++
+  console.log("Changed Age", age)
+}
+
+displayAge();
+changeAge();
+displayAge();
+changeAge();
+displayAge();
+changeAge();
