80d8699e129a09a3cf4a8012788249f754ec4200
suprajagangarajula7 <suprajagangarajula7@gmail.com>
2025-11-25 12:30:22 +0530

    counter

--- Week3Day3/Q20/20a.js
+++ Week3Day3/Q20/20a.js
@@ -15,3 +15,9 @@ const counter = createCounter();
 counter.increment(); 
 counter.increment(); 
 counter.decrement(); 
+
+///taska
+//A closure happens when an inner function remembers variables from its outer function—even after the outer function has finished executing.
+//What Happens if Multiple Counters Are Created?
+// Everytime when createCounter() is called it creates a new independent closure, meaning each counter has its own private copy of count.
+//so everytime we are getting different values of count. 
