ee9b018910c105ea56d849e1ea219e5044d0fbb2
suprajagangarajula7 <suprajagangarajula7@gmail.com>
2025-11-26 14:41:42 +0530

    order

--- Week3Day4/Q14/order.js
+++ Week3Day4/Q14/order.js
@@ -6,3 +6,8 @@ Promise.resolve().then(() => {
     console.log("Promise Task");
 });
 console.log("End");
+
+//output appears in this order because synchronous code runs first and also line by line. so "begin" and "end" are print on after other.
+//Promise callback goes to micro queue and always runs after synchronous tasks run before timer.
+//so we get promise task after "end"
+//setTimeout goes to macro task queue and run after promise is completed so thats why it is executed last.
